affe2306d50fc78d69388c1ee89fcb51b063d2a2
Садрутдинов Айдар <a.i.sadrutdinov@kalashnikovconcern.ru>
2021-09-06 09:57:02 +0400

    lesson 85. Добавление нового работника

--- src/main/java/com/sai/spring/rest/controller/MyRestController.java
+++ src/main/java/com/sai/spring/rest/controller/MyRestController.java
@@ -35,4 +35,11 @@ public class MyRestController {
         return employee;
     }
 
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
 }
